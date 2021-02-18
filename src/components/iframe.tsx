aad0b0c2891a9dcba7d9baf92269c4c3d1cdff45
Nismit <nismit.dev@gmail.com>
2021-02-17 22:22:36 -0800

    :zap: Update Iframe component

--- src/components/iframe.tsx
+++ src/components/iframe.tsx
@@ -2,17 +2,25 @@ type Props = {
     url: string
 }
 
+const iframeComponent = (url: string) => {
+    const code = `
+        <iframe 
+            width="100%" 
+            height="386" 
+            style="margin-bottom: 1rem;" 
+            src="${url}" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
+    `;
+
+    return {
+        __html: code
+    }
+}
+
 const Iframe: React.FC<Props> = ({ url }) => {
-    return (
-        <iframe
-            width="100%"
-            height="386"
-            style={{ marginBottom: '1rem' }}
-            src={url}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        />
-    )
+    return <div dangerouslySetInnerHTML={ iframeComponent(url) } />
 }
 
 export default Iframe;
