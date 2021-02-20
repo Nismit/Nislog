1d44b6471bff213ad2a334133425963411b51384
Nismit <nismit.dev@gmail.com>
2021-02-19 23:47:35 -0800

    :beetle: Fix Typo

--- src/pages/post/[...slug].tsx
+++ src/pages/post/[...slug].tsx
@@ -75,7 +75,7 @@ export async function getStaticProps({ params }: Params) {
     const slug = params.slug.join('/');
     const post = getPostBySlug(slug, [
         'title',
-        'desciption',
+        'description',
         'date',
         'lastmod',
         'slug',
