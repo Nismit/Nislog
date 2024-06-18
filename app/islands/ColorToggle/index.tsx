01652aeaac6a1d741b77876bb284248631c20ccf
Nismit <nismit.dev@gmail.com>
2024-06-17 21:12:19 -0700

    :sparkles: Add loader on theme toggle to prevent flickering

--- app/islands/ColorToggle/index.tsx
+++ app/islands/ColorToggle/index.tsx
@@ -1,4 +1,4 @@
-import { css } from "hono/css";
+import { css, keyframes } from "hono/css";
 import { useState, useEffect, useCallback } from "hono/jsx";
 
 const COLOR_THEME_KEY = "colorTheme";
@@ -50,8 +50,19 @@ const colorToggleClass = css`
   }
 `;
 
+const spinAnimation = keyframes`
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const loaderClass = css`
+  transform-origin: center;
+  animation: ${spinAnimation} 0.75s infinite linear;
+`;
+
 export default function ColorToggle() {
-  const [color, setColor] = useState<THEME_TYPE>(THEME_COLORS.LIGHT);
+  const [color, setColor] = useState<THEME_TYPE | undefined>(undefined);
 
   useEffect(() => {
     const root = window.document.documentElement.className as THEME_TYPE | null;
@@ -69,6 +80,23 @@ export default function ColorToggle() {
     },
     [color]
   );
+
+  if (!color) {
+    return (
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="currentColor"
+          d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+          class={loaderClass}
+        />
+      </svg>
+    );
+  }
 
   return (
     <button
