7cb20d81bfffb49b05f9e900126d5635975aa68b
Nismit <22581499+Nismit@users.noreply.github.com>
2026-01-04 14:06:00 -0800

    :sparkles: add trim trailing slash

--- app/server.ts
+++ app/server.ts
@@ -1,7 +1,11 @@
 import { showRoutes } from "hono/dev";
 import { createApp } from "honox/server";
+import { trimTrailingSlash } from "hono/trailing-slash";
 
 const app = createApp();
+
+// Remove trailing slashes from all routes
+app.use("*", trimTrailingSlash());
 
 showRoutes(app);
 
